f96797c0ca4cb89c859452fb49d478663382d59c
DawidRozewski <dawid.rrozewski@gmail.com>
2021-12-03 18:30:15 +0100

    Convert json hours to options in select.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,17 +2,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const dateInput = document.getElementById('pick_date');
     const hoursInput = document.getElementById('pick_hour');
 
+
     dateInput.addEventListener("change", function (event) {
         const pickedDate = event.target.value;
         fetch("http://localhost:8080/app/example-appointment/available?date=" + pickedDate)
+
             .then(resp => resp.json())
             .then(data => {
                 console.log(data)
-
+                hoursInput.length = 0;
                 data.forEach(element => {
-                    const select = document.createElement("select");
-                    select.innerText = element;
-                    hoursInput.appendChild(select);
+                    const option = document.createElement("option");
+                    option.text = element;
+                    hoursInput.appendChild(option);
                 })
             })
             .catch(error => {
--- src/main/webapp/WEB-INF/jsp/patient/appointment.jsp
+++ src/main/webapp/WEB-INF/jsp/patient/appointment.jsp
@@ -1,6 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8" %>
 <%@include file="temps/header.jsp" %>
-<!-- koontynuacja zmian żeby plik js był dostępny i możliwy do uruchomienia --->
 <script src="<c:url value="/app.js"/>" type="text/javascript"></script>
 <h2>Zaplanuj wizytę</h2>
 
@@ -22,8 +21,8 @@
     Wybierz datę: <form:input id="pick_date"   path="date" type="date" value="${today}"/>
                   <form:errors path="date" cssClass="error"/><br/>
 
-    Wybierz godzinę: <form:select id="pick_hour" path="time" items="${hoursDay}" /><br/>
+    Wybierz godzinę: <form:select id="pick_hour" path="time" items="${hoursDay}"/><br/>
 
     <input type="submit" value="Zarezerwuj">
-    
+
 </form:form>
