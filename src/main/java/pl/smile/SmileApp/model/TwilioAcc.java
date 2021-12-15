ce4c9dc805324641f1de5d67e4de63cc53264e60
DawidRozewski <dawid.rrozewski@gmail.com>
2021-12-15 18:29:15 +0100

    Prepare for Heroku.

--- src/main/java/pl/smile/SmileApp/model/TwilioAcc.java
+++ src/main/java/pl/smile/SmileApp/model/TwilioAcc.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class TwilioAcc {
-    private final String accSid =  System.getenv("TWILIO_ACCOUNT_SID");
-    private final String authToken = System.getenv("TWILIO_AUTH_TOKEN");
+    private final String TWILIO_ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    private final String TWILIO_AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
     private final String trialNumber = "+15073354937";
 
 }
