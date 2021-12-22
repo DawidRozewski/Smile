f154631da8a6c794530407ea4b3ec7fababb5fb1
DawidRozewski <dawid.rrozewski@gmail.com>
2021-12-22 18:31:55 +0100

    changed sms sending hour

--- src/main/java/pl/smile/SmileApp/model/ScheduledTasks.java
+++ src/main/java/pl/smile/SmileApp/model/ScheduledTasks.java
@@ -19,7 +19,7 @@ public class ScheduledTasks {
     private final TwilioAcc twilioAcc;
     private final AppointmentRepository appointmentRepository;
 
-    @Scheduled(cron = "0 00 19 * * *")
+    @Scheduled(cron = "0 35 19 * * *")
     public void sendSMS() {
         List<Appointment> appointments = appointmentRepository.findAll();
 
